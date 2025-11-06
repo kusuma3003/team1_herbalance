bb97491e7b57bf395b1cfadef6161c610ac5ac57
vino <shanvino.012710@gmail.com>
2025-11-05 23:29:58 -0500

    runner

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -7,14 +7,14 @@ import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin  = {"pretty","html:target/LMS.html",
+		plugin  = {"pretty","html:target/HER.html",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},  //cucumber reports
 		
 		monochrome = true,  //console output color and eliminate junk characters
 		tags = " ",  //tags from feature file
-		features = {"src/test/resources/Features_LMS"}, //location of feature files, we can give this way-->".//features//"
-		glue= {"stepDefinitions_LMS","hooks"}) //location of step definition files, gives granural level of info in test result
+		features = {"src/test/resources/features"}, //location of feature files, we can give this way-->".//features//"
+		glue= {"stepdefinition","Hooks"}) //location of step definition files, gives granural level of info in test result
 
 public class Runner {
 
